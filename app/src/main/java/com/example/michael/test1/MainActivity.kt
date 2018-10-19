91f750679c0e7b4661699b86523f6e3a1015845f
9mmichael <ueno.michael@gmail.com>
2018-10-19 15:34:02 +0900

    issues1

--- app/src/main/java/com/example/michael/test1/MainActivity.kt
+++ app/src/main/java/com/example/michael/test1/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //issues1
+        //issues1_te
     }
 }
